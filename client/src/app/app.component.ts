cc71fe115d85c82ac3cf2fdf5ef025a4a56e5999
Fernanda JB <klaupher@gmail.com>
2021-06-19 17:59:51 -0300

    Final da Sessão 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { IProduct } from './modules/product';
+import { IPagination } from './modules/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Skinet';
+  products!: IProduct[];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pagesize=50').subscribe(
+      (response: any) => {
+        this.products = response.data;
+      }, error => {
+        console.log(error);
+      });
+  }
 }
 
